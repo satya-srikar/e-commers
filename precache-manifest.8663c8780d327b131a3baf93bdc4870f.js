ac1aa8130e2348bf6754c9ce5f83260614c2c104
satya-srikar <satya-srikar@users.noreply.github.com>
2021-06-11 03:01:25 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.8663c8780d327b131a3baf93bdc4870f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d441dea274deaf17ecc894e912d85a",
+    "url": "/e-commers/index.html"
+  },
+  {
+    "revision": "dda9e371d491e2fe9639",
+    "url": "/e-commers/static/css/main.13820d97.chunk.css"
+  },
+  {
+    "revision": "c137a30c058e9144031d",
+    "url": "/e-commers/static/js/2.ced158aa.chunk.js"
+  },
+  {
+    "revision": "dda9e371d491e2fe9639",
+    "url": "/e-commers/static/js/main.e0ed25a5.chunk.js"
+  },
+  {
+    "revision": "3b95108b838ca0cce58d",
+    "url": "/e-commers/static/js/runtime~main.69d173ca.js"
+  },
+  {
+    "revision": "0581e393f5be942318c77242120b37d8",
+    "url": "/e-commers/static/media/crown.0581e393.svg"
+  },
+  {
+    "revision": "1420d0f7ca55a03d8c2f78ae0c60a8ce",
+    "url": "/e-commers/static/media/shopping-bag.1420d0f7.svg"
+  }
+]);
